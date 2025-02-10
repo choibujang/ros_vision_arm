2cabf38d6e849905c3306800b0a7e9665c968d20
bujangchoi <tiiptant@gmail.com>
2025-02-10 17:42:36 +0900

    mjpg udp

--- deli_cam_controller/src/deli_cam_controller.cpp
+++ deli_cam_controller/src/deli_cam_controller.cpp
@@ -3,9 +3,14 @@
 void DeliCamController::startCam() {
     this->start_cam = true;
 
-    this->pipe.start();
-    auto lastTime = std::chrono::high_resolution_clock::now();
+    std::shared_ptr<ob::Config> config = std::make_shared<ob::Config>();
+    config->enableVideoStream(OB_STREAM_COLOR);
 
+    std::cout << "Color frame format: " << colorFrame->format() << std::endl;
+
+    this->pipe.start(config);
+
+    uint32_t frame_id = 0; 
 
     while (this->start_cam) {
         auto frameSet = this->pipe.waitForFrames(100);
@@ -16,31 +21,39 @@ void DeliCamController::startCam() {
 
         auto colorFrame = frameSet->colorFrame();
 
-        auto now = std::chrono::high_resolution_clock::now();
+        std::vector<uint8_t> mjpeg_data((uint8_t*)colorFrame->data(), (uint8_t*)colorFrame->data() + colorFrame->dataSize());
 
-        if (std::chrono::duration_cast<std::chrono::milliseconds>(now - lastTime).count() >= 30) {
-            if(colorFrame) {
-                uint16_t *data = (uint16_t *)colorFrame->data();
+        int total_size = mjpeg_data.size();
+        int num_chunks = (total_size + max_chunk_size - 1) / max_chunk_size;
 
-                int width = colorFrame->width();
-                int height = colorFrame->height();
-                int frame_size = width * height * 3;
+        for (int i = 0; i < num_chunks; i++) {
+            int offset = i * max_chunk_size;
+            int chunk_size = std::min(max_chunk_size, total_size - offset);
 
-                std::vector<uint8_t> buffer(data, data + frame_size);
+            std::vector<uint8_t> packet(header_size + chunk_size);
 
-                ssize_t sent_len = sendto(this->sock, buffer.data(), buffer.size(), 0,
-                                (struct sockaddr *)&(this->server_addr), sizeof(this->server_addr));
-        
-                if (sent_len < 0) {
-                    std::cerr << "Sending failed! Error: " << strerror(errno) << std::endl;
-                } else {
-                    std::cout << "Frame sent: " << sent_len << " bytes" << std::endl;
-                }
+            memcpy(packet.data(), &frame_id, 4);
+            uint16_t chunk_idx = i;
+            uint16_t total_chunks = num_chunks;
+            memcpy(packet.data() + 4, &chunk_idx, 2);
+            memcpy(packet.data() + 6, &total_chunks, 2);
+
+            memcpy(packet.data() + header_size, mjpeg_data.data() + offset, chunk_size);
+
+            ssize_t sent_len = sendto(this->sock, packet.data(), packet.size(), 0,
+                                      (struct sockaddr *)&(this->server_addr), sizeof(this->server_addr));
+
+            if (sent_len < 0) {
+                std::cerr << "Sending failed! Error: " << strerror(errno) << std::endl;
+                break;
+            } else {
+            std::cout << "Frame sent: " << sent_len << " bytes" << std::endl;
             }
-                
         }
-        lastTime = now;
-    }        
+        sendto(this->sock, frame_end, strlen(frame_end), 0,
+               (struct sockaddr *)&(this->server_addr), sizeof(this->server_addr));
+        frame_id++;
+    }     
 }
 
 // std::vector<float> DeliCamController::calcCamCoordinatePose(int u, int v, int z) {
